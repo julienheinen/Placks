c78f61e6ef446bcfb8980430e0753a25737d6e85
Julien Heinen <julienheinen@hotmail.com>
2021-07-02 23:52:28 +0200

    Test

---
+++ Placks/Placks/Menu/Accueil.swift
@@ -0,0 +1,20 @@
+//
+//  Accueil.swift
+//  Placks
+//
+//  Created by Julien Heinen on 27/06/2021.
+//
+
+import SwiftUI
+
+struct Accueil: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Accueil_Previews: PreviewProvider {
+    static var previews: some View {
+        Accueil()
+    }
+}
